cb5b2a13b07bfb51b090a3caac3b083c6eed3b78
Lincoln Lee <git@lincoln.hk>
2014-05-30 00:51:27 +0800

    Add code to generate dark themes

--- generate_theme.rb
+++ generate_theme.rb
@@ -4,35 +4,31 @@ require 'tilt/erb'
 require 'pathname'
 require 'fileutils'
 
-template = Tilt::ERBTemplate.new('templates/dark/package.json.erb')
-
-output = template.render(self, title: "test", package_name: "testid")
-
-
-Dir.glob("ace/lib/ace/theme/*.js") do |file|
+Dir.glob('ace/lib/ace/theme/*.js') do |file|
   filename = Pathname.new(file).basename
+  theme_name = filename.to_s[0..-4].gsub('_', '.')
+  title = theme_name.gsub('.', ' ').split(' ').map(&:capitalize).join(' ')
+  package_name = 'cb.theme.' + theme_name
+  id = theme_name
+  # Remove previous theme
+  FileUtils.rm_rf(package_name) if File.directory?(package_name)
 
-  File.open(file, "r") do |f|
+  # Create theme directory
+  FileUtils.mkdir(package_name)
+  # Create ace directory
+  FileUtils.mkdir(package_name + '/ace')
+  # Copy theme.css
+  FileUtils.cp "ace/lib/ace/theme/#{filename.to_s[0..-4]}.css", "#{package_name}/ace/theme.css"
+
+  File.open(file, 'r') do |f|
     # White theme
-    f.grep(/isDark \= false/) do |match|
-
-      theme_name = filename.to_s[0..-4].gsub("_", ".")
-      title = theme_name.gsub(".", " ").split(" ").map(&:capitalize).join(" ")
-      package_name = "cb.theme." + theme_name
-      id = theme_name
-
-      # Remove previous theme
-      FileUtils.rm_rf(package_name) if File.directory?(package_name)
-
-      # Create theme directory
-      FileUtils.mkdir(package_name)
-
+    f.grep(/isDark \= false/) do |_match|
       # Prepare package.json content
       package_template = Tilt::ERBTemplate.new('templates/white/package.json.erb')
       package_content = package_template.render(self, title: title, package_name: package_name)
 
       # Write package.json content
-      File.open(package_name+"/package.json", "w") do |package|
+      File.open(package_name + '/package.json', 'w') do |package|
         package.write(package_content)
       end
 
@@ -41,24 +37,48 @@ Dir.glob("ace/lib/ace/theme/*.js") do |file|
       main_content = main_template.render(self, id: theme_name, title: title)
 
       # Write main.js content
-      File.open(package_name+"/main.js", "w") do |main|
+      File.open(package_name + '/main.js', 'w') do |main|
         main.write(main_content)
       end
 
-      # Create ace directory
-      FileUtils.mkdir(package_name+"/ace")
-
       # Prepare theme.js content
       theme_template = Tilt::ERBTemplate.new('templates/white/ace/theme.js.erb')
-      theme_content = theme_template.render(self, css_name: id.gsub(".", "-"))
+      theme_content = theme_template.render(self, css_name: id.gsub('.', '-'))
 
       # Write theme.js content
-      File.open(package_name+"/ace/theme.js", "w") do |theme|
+      File.open(package_name + '/ace/theme.js', 'w') do |theme|
         theme.write(theme_content)
       end
+    end
 
-      # Copy theme.css
-      FileUtils.cp "ace/lib/ace/theme/#{filename.to_s[0..-4]}.css", "#{package_name}/ace/theme.css"
+    # Dark theme
+    f.grep(/isDark \= true/) do |_match|
+      # Prepare package.json content
+      package_template = Tilt::ERBTemplate.new('templates/dark/package.json.erb')
+      package_content = package_template.render(self, title: title, package_name: package_name)
+
+      # Write package.json content
+      File.open(package_name + '/package.json', 'w') do |package|
+        package.write(package_content)
+      end
+
+      # Prepare main.js content
+      main_template = Tilt::ERBTemplate.new('templates/dark/main.js.erb')
+      main_content = main_template.render(self, id: theme_name, title: title)
+
+      # Write main.js content
+      File.open(package_name + '/main.js', 'w') do |main|
+        main.write(main_content)
+      end
+
+      # Prepare theme.js content
+      theme_template = Tilt::ERBTemplate.new('templates/dark/ace/theme.js.erb')
+      theme_content = theme_template.render(self, css_name: id.gsub('.', '-'))
+
+      # Write theme.js content
+      File.open(package_name + '/ace/theme.js', 'w') do |theme|
+        theme.write(theme_content)
+      end
     end
   end
 end
