01cb59ba620889139066c131ef50d1fbe897ed7d
Lincoln Lee <git@lincoln.hk>
2014-05-30 01:06:14 +0800

    Refactor generator

--- generate_theme.rb
+++ generate_theme.rb
@@ -22,63 +22,41 @@ Dir.glob('ace/lib/ace/theme/*.js') do |file|
 
   File.open(file, 'r') do |f|
     # White theme
-    f.grep(/isDark \= false/) do |_match|
-      # Prepare package.json content
-      package_template = Tilt::ERBTemplate.new('templates/white/package.json.erb')
-      package_content = package_template.render(self, title: title, package_name: package_name)
+    is_white = f.grep(/isDark \= true/).empty?
 
-      # Write package.json content
-      File.open(package_name + '/package.json', 'w') do |package|
-        package.write(package_content)
-      end
+    color = ""
 
-      # Prepare main.js content
-      main_template = Tilt::ERBTemplate.new('templates/white/main.js.erb')
-      main_content = main_template.render(self, id: theme_name, title: title)
-
-      # Write main.js content
-      File.open(package_name + '/main.js', 'w') do |main|
-        main.write(main_content)
-      end
-
-      # Prepare theme.js content
-      theme_template = Tilt::ERBTemplate.new('templates/white/ace/theme.js.erb')
-      theme_content = theme_template.render(self, css_name: id.gsub('.', '-'))
-
-      # Write theme.js content
-      File.open(package_name + '/ace/theme.js', 'w') do |theme|
-        theme.write(theme_content)
-      end
+    if is_white
+      color = "white"
+    else
+      color = "dark"
     end
 
-    # Dark theme
-    f.grep(/isDark \= true/) do |_match|
-      # Prepare package.json content
-      package_template = Tilt::ERBTemplate.new('templates/dark/package.json.erb')
-      package_content = package_template.render(self, title: title, package_name: package_name)
+    # Prepare package.json content
+    package_template = Tilt::ERBTemplate.new("templates/#{color}/package.json.erb")
+    package_content = package_template.render(self, title: title, package_name: package_name)
 
-      # Write package.json content
-      File.open(package_name + '/package.json', 'w') do |package|
-        package.write(package_content)
-      end
+    # Write package.json content
+    File.open(package_name + '/package.json', 'w') do |package|
+      package.write(package_content)
+    end
 
-      # Prepare main.js content
-      main_template = Tilt::ERBTemplate.new('templates/dark/main.js.erb')
-      main_content = main_template.render(self, id: theme_name, title: title)
+    # Prepare main.js content
+    main_template = Tilt::ERBTemplate.new("templates/#{color}/main.js.erb")
+    main_content = main_template.render(self, id: theme_name, title: title)
 
-      # Write main.js content
-      File.open(package_name + '/main.js', 'w') do |main|
-        main.write(main_content)
-      end
+    # Write main.js content
+    File.open(package_name + '/main.js', 'w') do |main|
+      main.write(main_content)
+    end
 
-      # Prepare theme.js content
-      theme_template = Tilt::ERBTemplate.new('templates/dark/ace/theme.js.erb')
-      theme_content = theme_template.render(self, css_name: id.gsub('.', '-'))
+    # Prepare theme.js content
+    theme_template = Tilt::ERBTemplate.new("templates/#{color}/ace/theme.js.erb")
+    theme_content = theme_template.render(self, css_name: id.gsub('.', '-'))
 
-      # Write theme.js content
-      File.open(package_name + '/ace/theme.js', 'w') do |theme|
-        theme.write(theme_content)
-      end
+    # Write theme.js content
+    File.open(package_name + '/ace/theme.js', 'w') do |theme|
+      theme.write(theme_content)
     end
   end
 end
